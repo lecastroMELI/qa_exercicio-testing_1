ba2b55377ca29e21490b34775a0bf85ca2056ea7
Letícia Castro <leticia.castro@mercadolivre.com>
2022-09-22 16:01:25 -0300

    Adicionado arquivo de configuração

---
+++ cucumber.js
@@ -0,0 +1,14 @@
+/**
+ * Share your Cucumber Report with your team at https://reports.cucumber.io
+ *
+ * Command line option:    --publish
+ * Environment variable:   CUCUMBER_PUBLISH_ENABLED=true
+ *
+ * More information at https://cucumber.io/docs/cucumber/environment-variables/ │
+ *
+ * To disable this message, add this to your ./cucumber.js:
+ * module.exports = { default: '--publish-quiet' }
+ */
+module.exports = {
+  default: '--publish-quiet',
+};
