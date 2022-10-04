bbbe6307f1136d28ea94402e6f91ee55db0d72fb
Letícia Castro <leticia.castro@mercadolivre.com>
2022-10-04 17:39:35 -0300

    Implementado GIVEN e WHEN para o novo cenário POST

--- features/steps/given.js
+++ features/steps/given.js
@@ -1,9 +1,19 @@
 const { Given } = require('@cucumber/cucumber');
+const body = require('../../lib/helpers/bodySample');
 
 Given('I start with {string}', function (initialMessage) {
-    this.initialMessage = initialMessage;
+  this.initialMessage = initialMessage;
 });
 
 Given('I have the code of an item: {string}', function (itemCode) {
   this.itemCode = itemCode;
+});
+
+Given('One item with the following attributes:', function (table) {
+  // const attrs = table.hashes(); TODO: POR NUM CONSOLE LOG PARA VER O QUE É
+  this.table = table;
+});
+
+Given('I have a sample POST body', function () {
+  this.body = body;
 });
--- features/steps/when.js
+++ features/steps/when.js
@@ -1,17 +1,26 @@
 const { When } = require('@cucumber/cucumber');
 
-const {itemsGET } = require('../../lib/api-controllers/itemAPI');
+const { itemsGET, itemPOST } = require('../../lib/api-controllers/itemAPI');
 
 When('I add {string}', function (complementMessage) {
-    this.complementMessage = complementMessage;
+  this.complementMessage = complementMessage;
 });
 
-When('I send a request', async function() {
-    itemCode = this.itemCode;
-    this.data = [];
+When('I send a request', async function () {
+  itemCode = this.itemCode;
+  this.data = [];
 
-    const response = await itemsGET(itemCode);
-    // console.log(response);
+  const response = await itemsGET(itemCode);
+  // console.log(response);
 
-    this.data.push(response);
+  this.data.push(response);
+});
+
+When('I send a request to Items with the sample body', async function () {
+  const { body } = this;
+  this.data = [];
+
+  const response = await itemPOST(body);
+  // console.log(response);
+  this.data.push(response);
 });
